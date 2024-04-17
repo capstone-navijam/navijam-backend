3a60d9a97497b6ae8f5b5f9c76289316aad19525
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-04-18 08:34:09 +0900

    feat: Swagger 적용

--- src/main/main.ts
+++ src/main/main.ts
@@ -4,9 +4,21 @@ import {
 import {
     AppModule,
 } from "./app.module";
+import {
+    DocumentBuilder, SwaggerModule,
+} from "@nestjs/swagger";
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
+    const options = new DocumentBuilder()
+        .setTitle("Navijam")
+        .setDescription("나비잠: 온라인 정신상담 웹 플랫폼")
+        .setVersion("1.0.0")
+        .build();
+
+    const document = SwaggerModule.createDocument(app, options);
+    SwaggerModule.setup("navijam", app, document);
+
     await app.listen(3000);
 }
 

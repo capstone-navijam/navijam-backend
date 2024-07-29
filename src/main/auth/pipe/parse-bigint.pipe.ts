0eec406959319240d6b51a8b10f43a87e0a44f6b
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-07-30 05:07:24 +0900

    feat: 요청 파라미터를 bigint 타입으로 변환하는 Pipe 설정

---
+++ src/main/auth/pipe/parse-bigint.pipe.ts
@@ -0,0 +1,14 @@
+import {
+    PipeTransform, Injectable, BadRequestException,
+} from "@nestjs/common";
+
+@Injectable()
+export class ParseBigIntPipe implements PipeTransform<string, bigint> {
+    transform(value: string): bigint {
+        try {
+            return BigInt(value);
+        } catch (e) {
+            throw new BadRequestException(`Invalid parameter: ${value} is not a valid BigInt`);
+        }
+    }
+}

0d042d95e64e9551d40a6bddbfa67a6dcfd7c24a
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-07-31 06:46:08 +0900

    docs: 응답 구조 변경

--- src/main/comfort/dto/res/get-comfort-board.response.dto.ts
+++ src/main/comfort/dto/res/get-comfort-board.response.dto.ts
@@ -18,23 +18,15 @@ export class GetComfortBoardResponseDto {
     readonly title: string;
 
     @ApiProperty({
-        type: String,
-        description: "게시글 내용",
-        example: "요즘 너무 지치는 일들이 많아 스트레스입니다.",
-    })
-    readonly content: string;
-
-    @ApiProperty({
         type: Date,
         description: "게시글 작성 시간",
         example: "2024-01-02T16:06:20.43672",
     })
     readonly createdAt: Date;
 
-    constructor(id: string, title: string, content: string, createdAt: Date) {
+    constructor(id: string, title: string, createdAt: Date) {
         this.id = id;
         this.title = title;
-        this.content = content;
         this.createdAt = createdAt;
     }
 }

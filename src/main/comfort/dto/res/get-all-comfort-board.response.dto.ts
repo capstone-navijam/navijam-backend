f6db83eb764a0b30c7673f52abd35b1f58f6e129
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-07-31 06:46:53 +0900

    feat: 위로받기 전체 조회 및 상세 조회에 필요한 응답 DTO 구현

---
+++ src/main/comfort/dto/res/get-all-comfort-board.response.dto.ts
@@ -0,0 +1,44 @@
+import {
+    ApiProperty,
+} from "@nestjs/swagger";
+import {
+    Category,
+} from "@main/global/category";
+
+export class GetAllComfortBoardResponseDto {
+    @ApiProperty({
+        type: String,
+        description: "Board Id",
+        example: "1",
+    })
+    readonly id: string;
+
+    @ApiProperty({
+        enum: Category,
+        description: "카테고리",
+        isArray: true,
+        example: ["FREE",],
+    })
+    readonly categories: Category[];
+
+    @ApiProperty({
+        type: String,
+        description: "게시글 제목",
+        example: "조언 부탁드립니다.",
+    })
+    readonly title: string;
+
+    @ApiProperty({
+        type: Date,
+        description: "게시글 작성 시간",
+        example: "2024-01-02T16:06:20.43672",
+    })
+    readonly createdAt: Date;
+
+    constructor(id: string, categories: Category[], title: string, createdAt: Date) {
+        this.id = id;
+        this.categories = categories;
+        this.title = title;
+        this.createdAt = createdAt;
+    }
+}

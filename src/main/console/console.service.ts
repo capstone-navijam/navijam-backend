50e04dcc77b5f0cc35a0f79c77994087aa06634a
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-08-27 16:23:23 +0900

    feat: 위로하기 전체 조회 API 구현

--- src/main/console/console.service.ts
+++ src/main/console/console.service.ts
@@ -1,7 +1,9 @@
 import {
+    ForbiddenException,
     Injectable,
 } from "@nestjs/common";
 import {
+    ComfortBoard,
     Console, Member,
     PrismaClient, Role,
 } from "@prisma/client";
@@ -15,6 +17,15 @@ import NotFoundBoardException from "@main/exception/not-found.board.exception";
 import {
     GetAllComfortBoardResponseDto,
 } from "@main/comfort/dto/res/get-all-comfort-board.response.dto";
+import {
+    prismaCategoryToCategory,
+} from "@main/global/category";
+import {
+    GetAllConsoleResponseDto,
+} from "@main/console/dto/res/get-all-console.response.dto";
+import {
+    CustomForbiddenExceptionFilter,
+} from "@main/filter/custom-forbidden-exception.filters";
 
 @Injectable()
 export class ConsoleService {
@@ -46,7 +57,7 @@ export class ConsoleService {
         return new WriteConsoleResponseDto(console.id.toString());
     }
 
-    // 위로하기 전체 조회
+    // 위로하기 답변 게시글 전체 조회
     async getAllBoards(listenerId: bigint): Promise<GetAllComfortBoardResponseDto[]> {
         const consoles = await this.prisma.console.findMany({
             where: {
@@ -69,11 +80,57 @@ export class ConsoleService {
             .filter((board): board is typeof boards[number] => board !== undefined);
 
         return uniqueBoards.map(
-            (board) =>
-                new GetAllComfortBoardResponseDto(
-                    board.id.toString(), board.title, board.createdAt
-                )
+            (board) => {
+                const categories = board.categories.map(prismaCategoryToCategory); // board.categories에 접근하여 매핑
+
+                return new GetAllComfortBoardResponseDto(
+                    board.id.toString(), categories, board.title, board.createdAt,
+                );
+            }
         );
     }
 
+    // 위로하기 전체 조회
+    async getAllConsoles(comfortBoardId: bigint, member: Member): Promise<GetAllConsoleResponseDto[]> {
+        const consoles = await this.prisma.console.findMany({
+            where: {
+                comfortId: comfortBoardId, // 특정 comfortBoardId에 맞는 답글만 가져옴
+            },
+            select: {
+                id: true,
+                content: true,
+                createdAt: true,
+                updatedAt: true,
+                comfort: {
+                    select: {
+                        id: true,
+                        memberId: true,
+                        title: true,
+                    },
+                },
+                member: {
+                    select: {
+                        id: true,
+                        nickname: true,
+                        profile: true,
+                    },
+                },
+            },
+        });
+        if (member.role === Role.LISTENER) {
+            return consoles.map(console => new GetAllConsoleResponseDto(
+                console.id.toString(), console.member?.nickname || "Anonymous", console.member?.profile || "default-profile-url", console.content, console.createdAt, console.updatedAt
+            ));
+        }
+
+        const isMemberAuthorized = consoles.some(console => console.comfort?.memberId === member.id);
+
+        if (!isMemberAuthorized) {
+            return consoles.map(console => new GetAllConsoleResponseDto(
+                console.id.toString(), console.member?.nickname || "Anonymous", console.member?.profile || "default-profile-url", console.content, console.createdAt, console.updatedAt
+            ));
+        } else {
+            throw new ForbiddenException();
+        }
+    }
 }

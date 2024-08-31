2781817d1f46dcd017173bc5d65cb93ed6917e8d
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-08-31 23:13:29 +0900

    fix: JWT 인증 과정 변경

--- src/main/auth/jwt/jwt-auth.guard.ts
+++ src/main/auth/jwt/jwt-auth.guard.ts
@@ -1,10 +1,32 @@
 import {
     Injectable,
+    ExecutionContext,
+    UnauthorizedException,
 } from "@nestjs/common";
 import {
     AuthGuard,
 } from "@nestjs/passport";
+import {
+    TokenExpiredError,
+} from "jsonwebtoken";
 
 @Injectable()
 export class JwtAuthGuard extends AuthGuard("navijam-token") {
+    handleRequest(err: any, member: any, info: any, context: ExecutionContext) {
+        if (err) {
+            if (err instanceof TokenExpiredError) {
+                throw new UnauthorizedException("로그인이 필요합니다.");
+            }
+            throw err;
+        }
+
+        if (!member) {
+            throw new UnauthorizedException("유효하지 않은 인증 정보입니다.");
+        }
+
+        const request = context.switchToHttp().getRequest();
+        request.member = member;
+
+        return member;
+    }
 }

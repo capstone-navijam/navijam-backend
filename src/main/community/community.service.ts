d0652437f8d474f2759b3ade46769b6426ce52ee
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-10-20 15:39:14 +0900

    feat: 커뮤니티 전체 조회 및 상세 조회 응답 DTO 및 API 구현

--- src/main/community/community.service.ts
+++ src/main/community/community.service.ts
@@ -2,19 +2,29 @@ import {
     Injectable,
 } from "@nestjs/common";
 import {
-    CommunityBoard,
     Member,
     PrismaClient,
+    CommunityBoard,
 } from "@prisma/client";
 import {
     WriteCommunityBoardRequestDto,
 } from "@main/community/dto/req/write-community-board.request.dto";
 import {
-    categoryMap,
+    categoryMap, prismaCategoryToCategory,
 } from "@main/global/category";
 import {
     WriteCommunityBoardResponseDto,
 } from "@main/community/dto/res/write-community-board.response.dto";
+import {
+    GetCommunityBoardDetailResponseDto,
+} from "@main/community/dto/res/get-community-board-detail.response.dto";
+import NotFoundBoardException from "@main/exception/not-found.board.exception";
+import {
+    getTimestamp,
+} from "@main/util/timestamp.util";
+import {
+    GetAllCommunityBoardResponseDto,
+} from "@main/community/dto/res/get-all-community-board.response.dto";
 
 @Injectable()
 export class CommunityService {
@@ -24,7 +34,7 @@ export class CommunityService {
     // 커뮤니티 글 작성
     async writeCommunity(member: Member, body: WriteCommunityBoardRequestDto): Promise<WriteCommunityBoardResponseDto> {
 
-        const board: CommunityBoard = await this.prisma.communityBoard.create({
+        const board = await this.prisma.communityBoard.create({
             data: {
                 title: body.title,
                 content: body.content,
@@ -36,4 +46,51 @@ export class CommunityService {
         return new WriteCommunityBoardResponseDto(board.id.toString());
     }
 
+    // 커뮤니티 전체 조회
+    async getAllCommunity(): Promise<GetAllCommunityBoardResponseDto[]> {
+        const boards = await this.prisma.communityBoard.findMany({
+            include: {
+                member: true,
+            },
+        });
+
+        return boards.map((board) => {
+
+            const categories = board.categories.map(prismaCategoryToCategory);
+
+            const memberProfile = board.member?.profile || "";
+            const memberNickname = board.member?.nickname || "";
+            const memberId = board.member?.id?.toString() || "";
+
+            const timestamp = getTimestamp(board.createdAt, board.updatedAt);
+            
+            return new GetAllCommunityBoardResponseDto(
+                board.id.toString(), memberProfile, memberNickname, categories, board.title, board.content, timestamp, memberId
+            );
+        });
+    }
+
+    // 커뮤니티 상세 조회
+    async getCommunityDetail(communityBoardId: bigint): Promise<GetCommunityBoardDetailResponseDto> {
+        const board = await this.prisma.communityBoard.findUnique({
+            where: {
+                id: communityBoardId,
+            },
+            include: {
+                member: true,
+            },
+        });
+
+        if (!board) {
+            throw new NotFoundBoardException;
+        }
+
+        const timestamp = getTimestamp(board.createdAt, board.updatedAt);
+
+        const categories = board.categories.map(prismaCategoryToCategory);
+
+        return new GetCommunityBoardDetailResponseDto(
+            board.id.toString(), board.member?.profile || "", board.member?.nickname || "", categories, board.title, board.content, board.memberId?.toString() || "", timestamp,
+        );
+    }
 }
---
+++ src/main/community/dto/res/get-community-board-detail.response.dto.ts
@@ -0,0 +1,85 @@
+import {
+    ApiProperty,
+} from "@nestjs/swagger";
+import {
+    Category,
+} from "@main/global/category";
+
+export class GetCommunityBoardDetailResponseDto {
+    @ApiProperty({
+        type: String,
+        description: "Board Id",
+        example: "1",
+    })
+    readonly id: string;
+
+    @ApiProperty({
+        type: String,
+        description: "작성자 프로필",
+        example: "https://example.com",
+    })
+    readonly profile: string;
+
+    @ApiProperty({
+        type: String,
+        description: "닉네임",
+        example: "example",
+    })
+    readonly nickname: string;
+
+    @ApiProperty({
+        description: "카테고리",
+        enum: Category,
+        isArray: true,
+        example: ["자유",],
+    })
+    readonly categories: Category[];
+
+    @ApiProperty({
+        type: String,
+        description: "커뮤니티 제목",
+        example: "조언 부탁드립니다.",
+    })
+    readonly title: string;
+
+    @ApiProperty({
+        type: String,
+        description: "커뮤니티 내용",
+        example: "요즘 너무 지치는 일들이 많아 스트레스입니다.",
+    })
+    readonly content: string;
+
+    @ApiProperty({
+        type: Date,
+        description: "작성 또는 수정된 시간",
+        example: "2024. 10. 03. 오후 06:56:15",
+    })
+    readonly timestamp: string;
+
+    @ApiProperty({
+        type: String,
+        description: "Member Id",
+        example: "1",
+    })
+    readonly memberId: string;
+
+    constructor(
+        id: string,
+        profile: string,
+        nickname: string,
+        categories: Category[],
+        title: string,
+        content: string,
+        memberId: string,
+        timestamp: string,
+    ) {
+        this.id = id;
+        this.profile = profile;
+        this.nickname = nickname;
+        this.categories = categories;
+        this.title = title;
+        this.content = content;
+        this.memberId = memberId;
+        this.timestamp = timestamp;
+    }
+}

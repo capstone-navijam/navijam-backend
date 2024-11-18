3aceef1a1982aeb1f1b84504689aa76d3e125b2a
seoyun-lee <20212025@o365.hanbat.ac.kr>
2024-11-18 15:01:57 +0900

    feat: 프로필 수정에 필요한 요청/응답 DTO 구현

---
+++ src/main/mypage/dto/req/update-member.profile.request.dto.ts
@@ -0,0 +1,40 @@
+import {
+    ApiProperty,
+} from "@nestjs/swagger";
+import {
+    IsNotEmpty, IsString, Matches,
+} from "class-validator";
+
+export class UpdateMemberProfileRequestDto {
+    @ApiProperty({
+        description: "닉네임",
+        required: true,
+    })
+    @IsString({})
+    @IsNotEmpty({
+        message: "공백을 포함할 수 없습니다.",
+    })
+    @Matches(/^[a-zA-Z0-9가-힣]{2,8}$/, {
+        message: "닉네임은 2~8글자의 한글 또는 영문자, 숫자여야 합니다.",
+    })
+    readonly nickname?: string;
+
+    @ApiProperty({
+        description: "비밀번호",
+        required: true,
+    })
+    @IsString({})
+    @IsNotEmpty({
+        message: "공백을 포함할 수 없습니다.",
+    })
+    @Matches(/^(?=.*\d)(?=.*[a-zA-Z])(?=.*[^a-zA-Z0-9]).{8,14}$/, {
+        message: "비밀번호는 8~14글자의 영문, 숫자, 특수문자를 포함해야 합니다.",
+    })
+    readonly newPassword?: string;
+
+    @ApiProperty({
+        description: "비밀번호 확인",
+        required: true,
+    })
+    readonly checkPassword?: string;
+}
